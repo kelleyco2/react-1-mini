9b5d61cccdb2b2f7980abfbf4007f2e36d3305c3
Cooper Kelley <cooperkelley@Coopers-MacBook-Pro.local>
2018-10-19 12:53:07 -0600

    Commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      picture: '',
+      friends: []
+    }
+  }
+
+  updatePicture(e){
+    let picture = e.target.value
+    this.setState({
+      picture
+    })
+  }
+
+  updateName(e){
+    let name = e.target.value
+    this.setState({
+      name
+    })
+  }
+
+  handleClick(){
+    let {name, picture} = this.state
+    let friend = {
+      name: name,
+      picture: picture
+    }
+    
+    let friends = this.state.friends
+    friends.push(friend)
+
+
+    this.setState({
+      friends
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Picture:</label>
+        <input onChange={e => this.updatePicture(e)}/>
+        <label>Name:</label>
+        <input onChange={e => this.updateName(e)}/>
+        <button onClick={() => this.handleClick()}>Add Friend</button>
+        {this.state.friends.map(friend => {
+          return (
+            <div>
+              <div>{friend.name}</div>
+              <img width={200} height={200} src={friend.picture}/>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
